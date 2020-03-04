ba41e748673ad36bb8a27f72c22b55ce8084e218
AO Industries, Inc. <support@aoindustries.com>
2020-03-03 23:28:28 -0600

    Registered style in ao-web-resources

--- src/main/java/com/semanticcms/dia/style/DiaStyle.java
+++ src/main/java/com/semanticcms/dia/style/DiaStyle.java
@@ -22,20 +22,28 @@
  */
 package com.semanticcms.dia.style;
 
+import com.aoindustries.web.resources.registry.Style;
+import com.aoindustries.web.resources.servlet.RegistryEE;
 import com.semanticcms.core.renderer.html.HtmlRenderer;
 import com.semanticcms.dia.model.Dia;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-@WebListener("Registers the styles for diagrams in HtmlRenderer.")
-public class Initializer implements ServletContextListener {
+@WebListener("Registers the styles for diagrams in RegistryEE and HtmlRenderer.")
+public class DiaStyle implements ServletContextListener {
+
+	public static final Style SEMANTICCMS_DIA = new Style("/semanticcms-dia-style/semanticcms-dia.css");
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(event.getServletContext());
+		ServletContext servletContext = event.getServletContext();
+
 		// Add our CSS file
-		htmlRenderer.addCssLink("/semanticcms-dia-style/semanticcms-dia.css");
+		RegistryEE.get(servletContext).global.styles.add(SEMANTICCMS_DIA);
+
+		HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(servletContext);
 		// Add link CSS class
 		htmlRenderer.addLinkCssClass(Dia.class, "semanticcms-dia-link");
 		// Add list item CSS class
