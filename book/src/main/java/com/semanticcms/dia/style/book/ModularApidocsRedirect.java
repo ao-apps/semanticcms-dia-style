f540912a646dd2b70915bac132a88a889eec19aa
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:44 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/dia/style/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/dia/style/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-dia-style - Default style for Java API for embedding Dia-based diagrams in web pages.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-dia-style.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.dia.style.book;
 
 import com.aoapps.net.URIParametersUtils;
