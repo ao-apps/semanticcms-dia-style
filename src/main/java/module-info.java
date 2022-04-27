3b765da3e72649558ed1b61ddec22494b2695b2e
AO Industries, Inc <support@aoindustries.com>
2022-04-27 00:04:52 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -30,4 +30,4 @@ module com.semanticcms.dia.style {
   requires com.semanticcms.dia.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-model</artifactId>
   // Transitive
   requires com.semanticcms.core.renderer; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer</artifactId>
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
